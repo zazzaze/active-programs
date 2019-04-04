4d7b162a238b0c06bc220f51453fbb6fc6b2ca0d
Егор Аникеев <egor.anikeev.00@gmail.com>
2019-04-05 00:18:25 +0500

    Сделана форма обратной свзяи

---
+++ src/js/popup.js
@@ -0,0 +1,14 @@
+const openPopup = document.querySelector('.content__button'),
+    closePopup = document.querySelector('.button__close');
+let popup = document.querySelector('.popup'),
+    body = document.querySelector('body');
+
+openPopup.addEventListener('click', () => {
+    popup.style.display = 'block';
+    body.style.overflow = 'hidden';
+});
+
+closePopup.addEventListener('click', () => {
+    popup.style.display = 'none';
+    body.style.overflow = '';
+});
