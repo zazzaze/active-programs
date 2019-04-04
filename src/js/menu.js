516d5f44d0bcdb1e17a0f1ddfa3e51b0ccd17534
Егор Аникеев <egor.anikeev.00@gmail.com>
2019-04-04 23:32:19 +0500

    Написан скрипт для menu

---
+++ src/js/menu.js
@@ -0,0 +1,13 @@
+const menuButton = document.querySelector('.burger');
+let menu = document.querySelector('.navbar--mobile');
+
+menuButton.addEventListener('click', () => {
+    console.log(menu.classList.contains('navbar--mobile-close'))
+    if (menu.classList.contains('navbar--mobile-close')){
+        menu.classList.remove('navbar--mobile-close');
+    } 
+    else {
+        menu.classList.add('navbar--mobile-close');  
+    }
+    
+})
