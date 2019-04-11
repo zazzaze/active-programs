053bbb45b87ab64644ba6776c9875cbfae4704da
Павел Макаров <nallden@yandex.ru>
2019-04-12 01:21:25 +0500

    я много сделал

---
+++ src/js/scroll.js
@@ -0,0 +1,12 @@
+ $(document).ready(function(){
+    $("#menu-header").on("click","a", function (event) {
+        alert();
+        document.querySelector('.navbar--mobile').classList.add('navbar--mobile-close');
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+            top-=90;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
