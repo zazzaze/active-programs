52c0e46d840d79f564f1c759e2325013ab8cdf26
Паша Макаров <nallden@yandex.ru>
2019-04-13 16:49:38 +0500

    fixed

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,17 +1,9 @@
  $(document).ready(function(){
-    $("#menu-header").on("click","a", function (event) {
+    $("#menu-header, #mobile-menu-header").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
             top-=0;
         $('body,html').animate({scrollTop: top}, 1500);
     });
-    $("#mobile-menu-header").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-            top-=0;
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-    
 });
