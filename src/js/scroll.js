e184fd1653d2d4fdc009cd79c205ba2ac643373d
Егор Аникеев <egor.anikeev.00@gmail.com>
2019-04-13 16:45:00 +0500

    123

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -6,4 +6,12 @@
             top-=0;
         $('body,html').animate({scrollTop: top}, 1500);
     });
+    $("#mobile-menu-header").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+            top-=0;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+    
 });
