a13b7e8a4016ff15d166ddf35100ca2128741ff7
Егор Аникеев <egor.anikeev.00@gmail.com>
2019-04-13 16:51:39 +0500

    оптимизирован скролл

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -6,12 +6,4 @@
             top-=0;
         $('body,html').animate({scrollTop: top}, 1500);
     });
-    $("#mobile-menu-header").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-            top-=0;
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-    
 });
