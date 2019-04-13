033b2965b93862fe7df7609788c4b74961cbed8b
Паша Макаров <nallden@yandex.ru>
2019-04-13 17:54:37 +0500

    zhopa

---
+++ src/js/bg.js
@@ -0,0 +1,5 @@
+// width=screen.width; // ширина  
+// height=screen.height; // высота
+
+// document.getElementById("fixed-background").style.width= width;
+// document.getElementById("fixed-background").style.height= height;
